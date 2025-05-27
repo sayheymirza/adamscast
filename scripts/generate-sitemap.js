3288bcea0f2b3d6152ec1d08d4a77ed2f1d68e5c
Mirza <sayheymirza@gamil.com>
2025-05-27 17:14:38 +0330

    ➕ sitemap and prerender episodes

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,40 @@
+const episodes = require('../public/episodes/database.json');
+
+const fs = require('fs');
+const path = require('path');
+
+const output = path.join(__dirname, '../public/sitemap.xml');
+const baseUrl = 'https://adamscast.ir';
+
+const paths = [
+    '/',
+    '/donate',
+    ...episodes.map(episode => {
+        return `/episode/${episode.slug}`;
+    })
+];
+
+const generateSitemap = () => {
+    const urls = paths.map(path => {
+        return `<url>
+    <loc>${baseUrl}${path}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.5</priority>
+</url>`;
+    }
+    ).join('\n');
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls}
+</urlset>`;
+
+    fs.writeFileSync(output, sitemap, 'utf8');
+    console.log(`Sitemap generated at ${output}`);
+}
+
+if (!fs.existsSync(path.dirname(output))) {
+    fs.mkdirSync(path.dirname(output), { recursive: true });
+}
+
+generateSitemap();
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,19 @@
+import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { EpisodesService } from './services/episodes.service';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+    path: 'episode/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const episodesService = inject(EpisodesService);
+
+      return episodesService.items.map(e => ({
+        slug: e.slug
+      }));
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
